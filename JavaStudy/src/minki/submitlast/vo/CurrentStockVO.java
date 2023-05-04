e39996f07670ee928137d2d393920c120076cfe4
qoralsrl03 <qoralsrl03@naver.com>
2023-05-04 18:54:06 +0900

    2023-05-04 18:53 자바 백업

--- JavaStudy/src/minki/submitlast/vo/CurrentStockVO.java
+++ JavaStudy/src/minki/submitlast/vo/CurrentStockVO.java
@@ -1,7 +1,9 @@
 package minki.submitlast.vo;
 
 public class CurrentStockVO {
-	private double lpr;				// 현재 주식 가격
-	private String stockname;		// 주식이름
+	private double clpr;				// 현재 주식 가격(2022-11-30)
+	private double hipr;				// 최고가
+	private double lopr;				// 최저가				
+	private String stockname;			// 주식이름
 	
 }
